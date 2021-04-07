1ccde2d4595a53733bcc16275a556233742b36b4
soy-braised-pork <2428784410@qq.com>
2021-04-07 20:20:10 +0800

    update

--- src/main/java/com/zlx/dao/UserDao.java
+++ src/main/java/com/zlx/dao/UserDao.java
@@ -8,14 +8,17 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao {
+    //插入
     @Insert("insert into User(id,username,password) values(#{id},#{username},#{password})")
     int insertUser(User user);
 
+    //更新
     @Update("upadte User set password=#{password} where username=#{username}")
-    int updateUser(String password,String username);
+    int updateUser(String password, String username);
 
+    //查找
     @Select("select*from User where username=#{username} and password=#{password}")
-    User selectByUsernameAndPassword(String username,String password);
+    User selectByUsernameAndPassword(String username, String password);
 
     @Select("select*from User where username=#{username}")
     User selectByUsername(String username);
